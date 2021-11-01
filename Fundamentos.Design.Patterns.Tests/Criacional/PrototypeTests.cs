c2186ea933524986e2da84ae49f857de83be9f67
Andre Luis Soares Alves Monteiro <andremonteiro.cb@hotmail.com>
2021-10-31 21:20:42 -0300

    Adicionando o pattern prototype;

---
+++ Fundamentos.Design.Patterns.Tests/Criacional/PrototypeTests.cs
@@ -0,0 +1,38 @@
+﻿using Fundamentos.Design.Patterns.Criacional.Prototype;
+using Xunit;
+
+namespace Desing.Patterns.Tests
+{
+    public class PrototypeTests
+    {
+        [Fact]
+        public void Prototype_CriarObjetosClonaveis_RetornarSucesso()
+        {
+            //Arrange
+            var pessoa = new Pessoa("User Test");
+            var pessoa2 = (Pessoa)pessoa.Clone();
+            
+            //Act
+            pessoa2.ChangeName("User Test Clonado");
+
+            //Assert
+            Assert.Equal("User Test", pessoa.Name);
+            Assert.Equal("User Test Clonado", pessoa2.Name);
+        }
+
+        [Fact]
+        public void Prototype_CriarObjetosClonaveis_RetornarFalha()
+        {
+            //Arrange
+            var pessoa = new Pessoa("User Test");
+            var pessoa2 = pessoa;
+
+            //Act
+            pessoa2.ChangeName("User Test Clonado");
+
+            //Assert
+            Assert.Equal("User Test Clonado", pessoa.Name);
+            Assert.Equal("User Test Clonado", pessoa2.Name);
+        }
+    }
+}
---
+++ Fundamentos.Design.Patterns/Criacional/Prototype/Pessoa.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Fundamentos.Design.Patterns.Criacional.Prototype
+{
+    public class Pessoa : ICloneable
+    {
+        public Guid Id { get; private set; }
+        public string Name { get; private set; }
+        public Pessoa(string name)
+        {
+            Id = Guid.NewGuid();
+            Name = name;
+        }
+
+        public void ChangeName(string newName)
+        {
+            Name = newName;
+        }
+
+        public object Clone()
+        {
+            return (Pessoa)this.MemberwiseClone();
+        }
+    }
+}
