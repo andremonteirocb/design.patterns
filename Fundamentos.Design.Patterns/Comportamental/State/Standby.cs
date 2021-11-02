3cf219ebaf2173a880793bb3b3696983d4df7d50
Andre Luis Soares Alves Monteiro <andremonteiro.cb@hotmail.com>
2021-11-02 12:14:01 -0300

    ajustes do tipo de padrão;

--- Fundamentos.Design.Patterns/Comportamental/State/Standby.cs
+++ Fundamentos.Design.Patterns/Comportamental/State/Standby.cs
@@ -1,10 +1,11 @@
-﻿using Fundamentos.Design.Patterns.Comportamental.State.Interfaces;
+﻿using Fundamentos.Design.Patterns.Comportamental.State.Context;
+using Fundamentos.Design.Patterns.Comportamental.State.Interfaces;
 
 namespace Fundamentos.Design.Patterns.Comportamental.State
 {
     public class Standby : IState
     {
-        public void Handle(Context.Machine context)
+        public void Handle(Machine context)
         {
             context.State = new Off();
         }
