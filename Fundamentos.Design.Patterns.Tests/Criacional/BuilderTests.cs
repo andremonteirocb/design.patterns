0d07c193b002a43106ca51ffd14319d3563a9357
Andre Luis Soares Alves Monteiro <andre.monteiro@phidelis.com.br>
2022-04-12 17:10:53 -0300

    criado o método builder;

---
+++ Fundamentos.Design.Patterns.Tests/Criacional/BuilderTests.cs
@@ -0,0 +1,25 @@
+using Fundamentos.Design.Patterns.Criacional.Builder;
+using Xunit;
+
+namespace Desing.Patterns.Tests
+{
+    public class BuilderTests
+    {
+        [Fact]
+        public void Builder_EnviarEmail_RetornarSucesso()
+        {
+            //Arrange
+            var builder = new EmailBuilder("emaildestino@gmail.com", "emailremetente@gmail.com", "assunto", "mensagem")
+                .AddSmtp("smtp.gmail.com", 587)
+                .AddCredentials("login", "senha")
+                .UseDefaultCredentials()
+                .UseEnabledSsl();
+
+            //Act
+            var result = builder.Send();
+
+            //Assert
+            Assert.True(result);
+        }
+    }
+}
---
+++ Fundamentos.Design.Patterns/Criacional/Builder/EmailBuilder.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections;
+using System.Net;
+using System.Net.Mail;
+using System.Net.Mime;
+using System.Text.RegularExpressions;
+
+namespace Fundamentos.Design.Patterns.Criacional.Builder
+{
+    public class EmailBuilder
+    {
+        public MailMessage Message { get; private set; }
+        public SmtpClient Smtp { get; private set; }
+        public EmailBuilder(string destinatario, string remetente, string assunto, string mensagem)
+        {
+            bool bValidaEmail = ValidaEnderecoEmail(destinatario);
+            if (bValidaEmail == false) throw new Exception("Email do destinatário inválido:" + destinatario);
+
+            Message = new MailMessage(remetente, destinatario, assunto, mensagem);
+        }
+
+        public void AddAnexos(ArrayList anexos)
+        {
+            foreach (string anexo in anexos)
+            {
+                Attachment anexado = new Attachment(anexo, MediaTypeNames.Application.Octet);
+                Message.Attachments.Add(anexado);
+            }
+        }
+
+        public EmailBuilder AddSmtp(string host, int port)
+        {
+            Smtp = new SmtpClient(host, port);
+            return this;
+        }
+
+        public EmailBuilder AddCredentials(string login, string senha)
+        {
+            Smtp.Credentials = new NetworkCredential(login, senha);
+            return this;
+        }
+
+        public EmailBuilder UseEnabledSsl()
+        {
+            Smtp.EnableSsl = true;
+            return this;
+        }
+
+        public EmailBuilder UseDefaultCredentials()
+        {
+            Smtp.UseDefaultCredentials = true;
+            return this;
+        }
+
+        public bool Send()
+        {
+            Smtp.Send(Message);
+            return true;
+        }
+
+        private static bool ValidaEnderecoEmail(string enderecoEmail)
+        {
+            try
+            {
+                //define a expressão regulara para validar o email
+                string texto_Validar = enderecoEmail;
+                Regex expressaoRegex = new Regex(@"\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}");
+
+                // testa o email com a expressão
+                if (expressaoRegex.IsMatch(texto_Validar))
+                {
+                    // o email é valido
+                    return true;
+                }
+                else
+                {
+                    // o email é inválido
+                    return false;
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
