d4d14402873e97e4f3df775ebb64802fffc3647d
Andre Luis Soares Alves Monteiro <andre.monteiro@phidelis.com.br>
2022-04-21 14:22:53 -0300

    pr;

--- Fundamentos.Design.Patterns.Tests/Criacional/BuilderTests.cs
+++ Fundamentos.Design.Patterns.Tests/Criacional/BuilderTests.cs
@@ -16,10 +16,10 @@ namespace Desing.Patterns.Tests
                 .UseEnabledSsl();
 
             //Act
-            //var result = builder.Send();
+            var result = builder.Send();
 
             //Assert
-            Assert.True(true);
+            Assert.True(result);
         }
     }
 }
