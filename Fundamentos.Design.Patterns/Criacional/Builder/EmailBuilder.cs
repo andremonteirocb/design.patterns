ffc559203ee1d7cd8cdeda7116e7648a0ade4658
Andre Luis Soares Alves Monteiro <andre.monteiro@phidelis.com.br>
2022-05-17 10:10:04 -0300

    ajustes nas variáveis do builder

--- Fundamentos.Design.Patterns/Criacional/Builder/EmailBuilder.cs
+++ Fundamentos.Design.Patterns/Criacional/Builder/EmailBuilder.cs
@@ -58,25 +58,12 @@ namespace Fundamentos.Design.Patterns.Criacional.Builder
             return true;
         }
 
-        private static bool ValidaEnderecoEmail(string enderecoEmail)
+        private static bool ValidaEnderecoEmail(string texto_validar)
         {
             try
             {
-                //define a expressão regulara para validar o email
-                string texto_Validar = enderecoEmail;
-                Regex expressaoRegex = new Regex(@"\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}");
-
-                // testa o email com a expressão
-                if (expressaoRegex.IsMatch(texto_Validar))
-                {
-                    // o email é valido
-                    return true;
-                }
-                else
-                {
-                    // o email é inválido
-                    return false;
-                }
+                var expressaoRegex = new Regex(@"\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}");
+                return expressaoRegex.IsMatch(texto_validar) ? true : false;
             }
             catch (Exception)
             {
